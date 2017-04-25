09b2bcbb69bd2857e8b2a45695bbd4784b455a1d
lowding <dyg1993@foxmail.com>
2017-04-25 23:02:37 +0800

    nothing serious

---
+++ src/sift.cpp
@@ -0,0 +1,23 @@
+/*************************************************************************
+    > File Name: sift.cpp
+    > Author: laoding
+    > Mail: dyg1993@foxmail.com 
+    > Created Time: 二  4/25 20:52:42 2017
+ ************************************************************************/
+
+#include<iostream>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+
+#include "common.h"
+
+using namespace std;
+using namespace cv;
+
+int main(int argc,char **argv)
+{
+	
+	return 0;
+}
+
